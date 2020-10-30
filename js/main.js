8d7a7c08c11b8ac2bc0b80f374ed1a336a2e68d4
boramcom <boramcom@daum.net>
2020-10-30 11:37:11 +0900

    태블릿용 스타일 시작

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function aname(vars) { //함수선언
 aname('김일국');//함수호출
 */
 $(document).ready(function(){
+	// 상단으로 바로가기 버튼 클릭
+	$(".to_top a").smoothScroll();
 	//alert('콜백함수 실행');
 	$(".openMOgnb").click(function(){
 		//alert();//디버그용
